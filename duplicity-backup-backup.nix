b0c9e7d7b8622797efc3edbbe2e50292baedc853
Adrian Parvin D. Ouano <adrianparvino@gmail.com>
2019-01-06 07:16:19 +0800

    Factor backup implementation into its own file

---
+++ duplicity-backup-backup.nix
@@ -0,0 +1,62 @@
+{ pkgs, config, lib, ... }:
+
+with lib;
+
+let
+  gcfg = config.services.duplicity-backup;
+in
+{
+  config = mkIf gcfg.enable {
+    systemd.services =
+      mapAttrs' (name: cfg: nameValuePair "duplicity-${name}" {
+        description = "Duplicity archive '${name}'";
+        requires    = [ "network-online.target" ];
+        after       = [ "network-online.target" ];
+
+        path = with pkgs; [ gnupg ];
+
+        # make sure that the backup server is reachable
+        #preStart = ''
+        #  while ! ping -q -c 1 ${findawaytoextracttheaddressmaybe} &> /dev/null; do sleep 3; done
+        #'';
+
+        script = ''
+          for i in ${gcfg.envDir}/*; do
+             source $i
+          done
+
+          mkdir -p ${gcfg.cachedir}
+          chmod 0700 ${gcfg.cachedir}
+
+          ${concatStringsSep "\n" (map (directory: ''
+            ${pkgs.duplicity}/bin/duplicity \
+              --archive-dir ${gcfg.cachedir} \
+              --name ${name}-${baseNameOf directory} \
+              --gpg-options "--homedir=${gcfg.pgpDir}" \
+            '' + optionalString (!gcfg.usePassphrase) ''--encrypt-key "Duplicity Backup" \'' +
+            ''
+              ${concatStringsSep " " (map (v: "--exclude ${v}") cfg.excludes)} \
+              ${concatStringsSep " " (map (v: "--include ${v}") cfg.includes)} \
+              ${directory} \
+              ${cfg.destination}/${baseNameOf directory}
+            '') cfg.directories)}
+        '';
+
+        serviceConfig = {
+          Type = "oneshot";
+          IOSchedulingClass = "idle";
+          NoNewPrivileges = "true";
+          CapabilityBoundingSet = [ "CAP_DAC_READ_SEARCH" ];
+          PermissionsStartOnly = "true";
+        };
+      }) gcfg.archives;
+
+    # Note: the timer must be Persistent=true, so that systemd will start it even
+    # if e.g. your laptop was asleep while the latest interval occurred.
+    systemd.timers = mapAttrs' (name: cfg: nameValuePair "duplicity-${name}"
+      { timerConfig.OnCalendar = cfg.period;
+        timerConfig.Persistent = "true";
+        wantedBy = [ "timers.target" ];
+      }) gcfg.archives;
+  };
+}
--- duplicity-backup.nix
+++ duplicity-backup.nix
@@ -64,7 +64,7 @@ let
   '') gcfg.archives;
 in
 {
-  imports = [ ./duplicity-backup-options.nix ];
+  imports = [ ./duplicity-backup-options.nix ./duplicity-backup-backup.nix ];
 
   config = mkIf gcfg.enable {
     warnings = concatLists (mapAttrsToList (name: cfg:
@@ -77,58 +77,6 @@ in
           message = "Must specify paths for duplicity to back up";
         }) gcfg.archives);
 
-    systemd.services =
-      mapAttrs' (name: cfg: nameValuePair "duplicity-${name}" {
-        description = "Duplicity archive '${name}'";
-        requires    = [ "network-online.target" ];
-        after       = [ "network-online.target" ];
-
-        path = with pkgs; [ gnupg ];
-
-        # make sure that the backup server is reachable
-        #preStart = ''
-        #  while ! ping -q -c 1 ${findawaytoextracttheaddressmaybe} &> /dev/null; do sleep 3; done
-        #'';
-
-        script = ''
-          for i in ${gcfg.envDir}/*; do
-             source $i
-          done
-
-          mkdir -p ${gcfg.cachedir}
-          chmod 0700 ${gcfg.cachedir}
-
-          ${concatStringsSep "\n" (map (directory: ''
-            ${pkgs.duplicity}/bin/duplicity \
-              --archive-dir ${gcfg.cachedir} \
-              --name ${name}-${baseNameOf directory} \
-              --gpg-options "--homedir=${gcfg.pgpDir}" \
-            '' + optionalString (!gcfg.usePassphrase) ''--encrypt-key "Duplicity Backup" \'' +
-            ''
-              ${concatStringsSep " " (map (v: "--exclude ${v}") cfg.excludes)} \
-              ${concatStringsSep " " (map (v: "--include ${v}") cfg.includes)} \
-              ${directory} \
-              ${cfg.destination}/${baseNameOf directory}
-            '') cfg.directories)}
-        '';
-
-        serviceConfig = {
-          Type = "oneshot";
-          IOSchedulingClass = "idle";
-          NoNewPrivileges = "true";
-          CapabilityBoundingSet = [ "CAP_DAC_READ_SEARCH" ];
-          PermissionsStartOnly = "true";
-        };
-      }) gcfg.archives;
-
-    # Note: the timer must be Persistent=true, so that systemd will start it even
-    # if e.g. your laptop was asleep while the latest interval occurred.
-    systemd.timers = mapAttrs' (name: cfg: nameValuePair "duplicity-${name}"
-      { timerConfig.OnCalendar = cfg.period;
-        timerConfig.Persistent = "true";
-        wantedBy = [ "timers.target" ];
-      }) gcfg.archives;
-
-    environment.systemPackages = [ pkgs.duplicity duplicityGenKeys ] ++ restoreScripts;
+    environment.systemPackages = restoreScripts;
   };
 }
