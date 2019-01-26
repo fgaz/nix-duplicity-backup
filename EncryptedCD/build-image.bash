e645e3fac1310f759f87a6565e48d5beef307f8b
Adrian Parvin D. Ouano <adrianparvino@gmail.com>
2019-01-27 01:53:15 +0800

    Move EncryptedCD files to their own repository

--- EncryptedCD/build-image.bash
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-bash ./wrap-squash-in-luks.bash
-nix-build -E '((import <nixpkgs/nixos> {}).config.system.build.isoImage)' -I nixos-config=./iso.nix --option cores 4 --substituters ''
