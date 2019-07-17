31a7f1335eccb9ed2217280d80d7b2ea7cadfd9a
Adrian Parvin D. Ouano <adrianparvino@gmail.com>
2019-07-18 03:22:05 +0800

    Revert using instanced services
    Instanced services do not play well when the service is wantedBy
    multi-user.target

--- duplicity-backup-backup.nix
+++ duplicity-backup-backup.nix
@@ -13,62 +13,59 @@ in
     services.duplicity-backup.archives = mkOption {
       type = types.attrsOf (types.submodule ({ name, config, ... }: {
         options = {
-          backup-script = mkOption {
-            type = types.path;
+          backupService = mkOption {
+            type = types.attrs;
           };
         };
 
-        config.backup-script = pkgs.writeScriptBin "duplicity-${name}" (''
-          for i in ${gcfg.envDir}/*; do
-              source $i
-          done
+        config.backupService = {
+          description = "Duplicity archive ${name}";
 
-          mkdir -p ${gcfg.cacheDir}
-          chmod 0700 ${gcfg.cacheDir}
+          requires    = [ "network-online.target" ];
+          after       = [ "network-online.target" ];
 
-          ${pkgs.duplicity}/bin/duplicity \
-            --archive-dir ${gcfg.cacheDir} \
-            --name ${name} \
-            --gpg-options "--homedir=${gcfg.pgpDir}" \
-            --full-if-older-than 1M \
-        '' + optionalString (config.allowSourceMismatch) ''--allow-source-mismatch \
-        '' + optionalString (!gcfg.usePassphrase) ''--encrypt-key "Duplicity Backup" \
-        '' + ''
-            ${concatStringsSep " " (map (v: "--include '${v}'") config.includes)} \
-            ${concatStringsSep " " (map (v: "--exclude '${v}'") config.excludes)} \
-            ${config.directory} \
-            ${config.destination}
-        '');
+          serviceConfig = {
+            Type = "oneshot";
+            IOSchedulingClass = "idle";
+            NoNewPrivileges = "true";
+            CapabilityBoundingSet = [ "CAP_DAC_READ_SEARCH" ];
+            PermissionsStartOnly = "true";
+          };
+
+          script = ''
+            for i in ${gcfg.envDir}/*; do
+                source $i
+            done
+
+            mkdir -p ${gcfg.cacheDir}
+            chmod 0700 ${gcfg.cacheDir}
+
+            ${pkgs.duplicity}/bin/duplicity \
+              --archive-dir ${gcfg.cacheDir} \
+              --name ${name} \
+              --gpg-options "--homedir=${gcfg.pgpDir}" \
+              --full-if-older-than 1M \
+          '' + optionalString (config.allowSourceMismatch) ''--allow-source-mismatch \
+          '' + optionalString (!gcfg.usePassphrase) ''--encrypt-key "Duplicity Backup" \
+          '' + ''
+              ${concatStringsSep " " (map (v: "--include '${v}'") config.includes)} \
+              ${concatStringsSep " " (map (v: "--exclude '${v}'") config.excludes)} \
+              ${config.directory} \
+              ${config.destination}
+          '';
+        };
       }));
     };
   };
 
   config = mkIf (gcfg.enable && gcfg.enableBackup) {
-    systemd.services."duplicity@" = {
-      description = "Duplicity archive '%i'";
-
-      environment.ARCHIVE = "duplicity-%i";
-      path = mapAttrsToList (_: cfg: cfg.backup-script) gcfg.archives;
-
-      script = ''
-        exec ''${ARCHIVE/:/ }
-      '';
-
-      requires    = [ "network-online.target" ];
-      after       = [ "network-online.target" ];
-
-      serviceConfig = {
-        Type = "oneshot";
-        IOSchedulingClass = "idle";
-        NoNewPrivileges = "true";
-        CapabilityBoundingSet = [ "CAP_DAC_READ_SEARCH" ];
-        PermissionsStartOnly = "true";
-      };
-    };
+    systemd.services = mapAttrs' (name: archive:
+      nameValuePair "duplicity-${name}" archive.backupService
+    ) gcfg.archives;
 
     # Note: the timer must be Persistent=true, so that systemd will start it even
     # if e.g. your laptop was asleep while the latest interval occurred.
-    systemd.timers = mapAttrs' (name: cfg: nameValuePair "duplicity@${name}"
+    systemd.timers = mapAttrs' (name: cfg: nameValuePair "duplicity-${name}"
       { timerConfig.OnCalendar = cfg.period;
         timerConfig.Persistent = "true";
         wantedBy = [ "timers.target" ];
